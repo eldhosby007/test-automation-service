c1e7cdf77836d8b992e7f9f36da84c8fe55655e6
eldhosby007 <eldhosby007@gmail.com>
2025-05-18 20:50:35 +0530

    pushed changes

--- Class1.cs
+++ Class1.cs
@@ -4,5 +4,6 @@ namespace TestAutomationService
     public class Class1
     {
         public string Hello() => "Hello from TestAutomationService!";
+        public string Success() => "Success build";
     }
 }
